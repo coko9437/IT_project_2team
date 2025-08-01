db376f882e758b2baf86d64529e86f620d9f1d58
coko9437 <coko9437@gmail.com>
2025-08-01 17:50:33 +0900

    250801) 관리자 데이터 : admin/12341234 8교시

---
+++ src/main/generated/webproject_2team/lunch_matching/domain/QInquiry.java
@@ -0,0 +1,51 @@
+package webproject_2team.lunch_matching.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QInquiry is a Querydsl query type for Inquiry
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QInquiry extends EntityPathBase<Inquiry> {
+
+    private static final long serialVersionUID = -388902816L;
+
+    public static final QInquiry inquiry = new QInquiry("inquiry");
+
+    public final StringPath answer = createString("answer");
+
+    public final DateTimePath<java.time.LocalDateTime> answeredAt = createDateTime("answeredAt", java.time.LocalDateTime.class);
+
+    public final StringPath content = createString("content");
+
+    public final DateTimePath<java.time.LocalDateTime> createdAt = createDateTime("createdAt", java.time.LocalDateTime.class);
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final BooleanPath isAnswered = createBoolean("isAnswered");
+
+    public final StringPath title = createString("title");
+
+    public final StringPath writer = createString("writer");
+
+    public QInquiry(String variable) {
+        super(Inquiry.class, forVariable(variable));
+    }
+
+    public QInquiry(Path<? extends Inquiry> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QInquiry(PathMetadata metadata) {
+        super(Inquiry.class, metadata);
+    }
+
+}
+
---
+++ src/main/generated/webproject_2team/lunch_matching/domain/QNotice.java
@@ -0,0 +1,47 @@
+package webproject_2team.lunch_matching.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QNotice is a Querydsl query type for Notice
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QNotice extends EntityPathBase<Notice> {
+
+    private static final long serialVersionUID = 1239980319L;
+
+    public static final QNotice notice = new QNotice("notice");
+
+    public final StringPath content = createString("content");
+
+    public final DateTimePath<java.time.LocalDateTime> createdAt = createDateTime("createdAt", java.time.LocalDateTime.class);
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath title = createString("title");
+
+    public final NumberPath<Integer> viewCount = createNumber("viewCount", Integer.class);
+
+    public final StringPath writer = createString("writer");
+
+    public QNotice(String variable) {
+        super(Notice.class, forVariable(variable));
+    }
+
+    public QNotice(Path<? extends Notice> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QNotice(PathMetadata metadata) {
+        super(Notice.class, metadata);
+    }
+
+}
+
